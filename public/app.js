0434a6bd33c0f34849030e946b476dc4d03c4d4a
John Ahn <john.yo.ahn@gmail.com>
2020-11-19 14:04:01 -0800

    START Querying with Models

--- public/app.js
+++ public/app.js
@@ -1,8 +1,29 @@
 var express = require("express");
 var app = express();
+const { db, Page, User } = require('./models');
+const PORT = 3000;
 
-app.get("/", function (req, res) {
-  res.send("hello world");
+app.get("/", function(req, res) {
+    res.send("hello world");
 });
 
-app.listen(3000);
+db.authenticate()
+    .then(() => {
+        console.log('connected to the database');
+    })
+
+
+const init = async() => {
+    await db.sync({ force: false })
+    await Page.sync();
+    await User.sync();
+    // make sure that you have a PORT constant
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}!`);
+    });
+}
+
+init();
+
+
+// app.listen(3000);
